925435665ba6fcc9a7ba3b012f823a67ee14ef02
anrwn <andrenrwn@users.noreply.github.com>
2023-09-20 04:00:36 +0800

    Modified the API tester to use Google API with a fix for CORS errors by using https://corsanywhere.herokuapp.com

--- test/api_test/assets/js/homepage.js
+++ test/api_test/assets/js/homepage.js
@@ -2,19 +2,24 @@ var userFormEl = document.querySelector("#user-form");
 var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var mySearchTerm = document.querySelector("#my-search-term");
+var myFetchUrl = document.querySelector("#api-url");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  var username = nameInputEl.value.trim();
+  var searchterm = nameInputEl.value.trim();
 
-  if (username) {
+  if (searchterm) {
     repoContainerEl.textContent = "";
     nameInputEl.value = "";
-    getYelpSearch(username);
+    if (document.getElementById("syelp").checked) {
+      getYelpSearch(searchterm);
+    } else {
+      getGoogleSearch(searchterm);
+    }
   } else {
-    alert("Please enter a Yelp search term");
+    alert("Please enter a search term");
   }
 };
 
@@ -36,62 +41,52 @@ var getYelpSearch = function (search) {
   //      --url https://api.yelp.com/v3/events?categories=barcrawl
   //      --header 'Authorization: Bearer API_KEY'
   //      --header 'accept: application/json'
+
   // YELP API for businesses:
-  // curl -vvvk --request GET
-  //      --url "https://api.yelp.com/v3/businesses/search?term=bar&latitude=30.2849231&longitude=-97.7366316&radius=5000&limit=20"
-  //      --header 'Authorization: Bearer API_KEY'
-  //      --header 'accept: application/json' | json_pp > yelpbusinesssearch.json
+  /*
+  curl -vvvk --request GET --header 'Origin: null' \
+             --header 'Connection: close' \
+             --header 'Accept: application/json' \
+             --header 'Authorization: Bearer API_KEY' \
+             'https://api.yelp.com/v3/businesses/search?term=bar&latitude=30.2849231&longitude=-97.7366316&radius=5000&limit=20'
+  */
+
+  // YELP API for businesses example via CORS proxy:
+  /*
+  curl -vvvk --request GET --header 'Origin: null' \
+             --header 'Connection: close' \
+             --header 'Accept: application/json' \
+             --header 'Authorization: Bearer API_KEY' \
+             'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bar&latitude=30.2849231&longitude=-97.7366316&radius=5000&limit=20'
+  */
 
   var latitude = 30.2849231;
   var longitude = -97.7366316;
   var radius = 5000; //5km
   //var apiUrl = "https://api.yelp.com/v3/businesses/search?term" + search;
   var apiUrl =
-    "https://api.yelp.com/v3/events?limit=20&categories=" +
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" +
     search +
     "&latitude=" +
     latitude +
     "&longitude=" +
     longitude +
     "&radius=" +
-    radius;
+    radius +
+    "&limit=20";
 
   console.log("will fetch ", apiUrl);
-//  var data = JSON.parse(yelp_b_searchexample);
-//  console.log(data);
-//  displayYelpBusinesses(data, search);
+  myFetchUrl.textContent = apiUrl;
 
-  /* Current fetch has problems due to CORS */
-
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer E_PPY2I4Kj_RTIlu5iBCzVSdbNhgM8698ypfBPe7-KM3JkJfHtxBjWd5x6KOuvG_2OORfPPl-wb5vpDrVgbttjhU2F7aC2OJATUzhX6CDa0_i5KL2zneMFcyuLUIZXYx");
-  myHeaders.append("Cookie", "__cfduid=db290300ecfe95ec1fe3bc92c388c3c991586618117");
-  myHeaders.append("Access-Control-Allow-Origin", "*");
-  
-  var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-  // var targetUrl = 'https://api.yelp.com/v3/businesses/search?location=Houston'
-  var targetUrl = 'https://api.yelp.com/v3/businesses/search?term=bar&latitude=30.2849231&longitude=-97.7366316&radius=5000&limit=20';
-  
-  var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-  
-  fetch(proxyUrl + targetUrl, requestOptions)
-/*
   fetch(apiUrl, {
     method: "GET", // POST, PUT, DELETE, etc.
     //mode: 'cors',
     headers: {
-      "x-requested-with": "xmlhttprequest",
-      "Access-Control-Allow-Origin": "*",
-      // "Content-Type": "text/plain;charset=UTF-8"
-      "Authorization": "Bearer " + myapikeys.yelp, // API key is in headers for Yelp API
-      "Accept": "application/json",
-      "redirect": 'follow'
+      Authorization: "Bearer " + myapikeys.yelp, // API key is in headers for Yelp API
+      Accept: "application/json",
+      Origin: "null",
     },
-  }) */
+  })
     .then(function (response) {
       if (response.ok) {
         console.log(response);
@@ -106,7 +101,69 @@ var getYelpSearch = function (search) {
     .catch(function (error) {
       alert("Unable to connect to Yelp");
     });
-    /**/
+};
+
+var getGoogleSearch = function (search) {
+  var latitude = 30.2849231;
+  var longitude = -97.7366316;
+  var radius = 5000; //5km
+
+  // Google Places API documentation
+  // https://developers.google.com/maps/documentation/places/web-service/search-find-place#maps_http_places_findplacefromtext_mca-txt
+  // https://developers.google.com/maps/documentation/places/web-service/search-nearby
+
+  // Example google Place API call
+  // Text search
+  // curl -L -X GET 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars%20in%20Austin&key=YOUR_API_KEY'
+  //
+  // Find Place request for "bars", using the locationbias parameter to prefer results within 2000 meters of the specified coordinates
+  // curl -L -X GET 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian&inputtype=textquery&locationbias=circle%3A2000%4047.6918452%2C-122.2226413&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=YOUR_API_KEY'
+  //
+  // Search Nearby  <=== This is probably what we should use, searches 20 items per page
+  // curl -L -X GET 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.2849231%2C-97.7366316&radius=3000&keyword=bar&key=YOUR_API_KEY' -o test/yelp_api_test/googlenearbykeywordsearch2.json
+  //
+  // Place Photo API
+  // curl -v -L -X GET 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=ATJ83zgjwIr7QgnwAzhRmWt9sS1vo3_T7vARZ0Q-rpyE0_C0cYXKp8TU26Kyox_uM1JZrjkPtoQUORW4cZrI9njShYAArFUVvWvIdITXAKVtJVVZ9naigktLd9L88nLDgSAA6kaZqzkgzDuKIn98zwCqaDE9KFXbxZUcDfEsNRVpRbRqTz4d&key=YOUR_API_KEY'
+
+  var apiUrl =
+  // "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" +  // This gives a CORS error
+  "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" +
+    search +
+    "&location=" +
+    latitude +
+    "%2C" +
+    longitude +
+    "&radius=" +
+    radius +
+    "&key=" +
+    myapikeys.google;
+
+  console.log("will fetch ", apiUrl);
+  myFetchUrl.textContent = apiUrl;
+
+  fetch(apiUrl, {
+    method: "GET", // POST, PUT, DELETE, etc.
+    //mode: 'cors',
+    headers: {
+      Origin: "null",
+      "Accept-Language": "en, *",
+      Accept: "application/json",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayGoogleBusinesses(data, "user");
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Yelp");
+    });
 };
 
 var getUserRepos = function (user) {
@@ -152,7 +209,7 @@ var displayYelpBusinesses = function (data, searchTerm) {
     return;
   }
 
-  repoSearchTerm.textContent = searchTerm;
+  mySearchTerm.textContent = searchTerm;
 
   for (var i = 0; i < data.businesses.length; i++) {
     // The result will be `<github-username>/<github-repository-name>`
@@ -176,8 +233,50 @@ var displayYelpBusinesses = function (data, searchTerm) {
         categories += data.businesses[i].categories[j].alias + " ";
         console.log(data.businesses[i].categories[j].alias);
       }
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-sunglasses'></i>" + categories;
+      statusEl.innerHTML = "<i class='fas fa-times status-icon icon-sunglasses'></i>" + categories;
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-sunglasses'></i>";
+    }
+
+    repoEl.appendChild(statusEl);
+
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+// render Google results
+var displayGoogleBusinesses = function (data, searchTerm) {
+  if (data.results.length === 0) {
+    repoContainerEl.textContent = "No businesses found.";
+    // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
+    return;
+  }
+
+  mySearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < data.results.length; i++) {
+    // The result will be `<github-username>/<github-repository-name>`
+    var businessName = data.results[i].name;
+
+    console.log(businessName);
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    var titleEl = document.createElement("span");
+    titleEl.textContent = businessName;
+
+    repoEl.appendChild(titleEl);
+
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    if (data.results[i].types.length > 0) {
+      let categories = "";
+      for (var j = 0; j < data.results[i].types.length; j++) {
+        categories += data.results[i].types[j] + " ";
+        console.log(data.results[i].types[j]);
+      }
+      statusEl.innerHTML = "<i class='fas fa-times status-icon icon-sunglasses'></i>" + categories;
     } else {
       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-sunglasses'></i>";
     }
@@ -195,7 +294,7 @@ var displayRepos = function (repos, searchTerm) {
     return;
   }
 
-  repoSearchTerm.textContent = searchTerm;
+  mySearchTerm.textContent = searchTerm;
 
   for (var i = 0; i < repos.length; i++) {
     // The result will be `<github-username>/<github-repository-name>`
