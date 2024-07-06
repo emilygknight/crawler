f602f6850a290a01a7a65630c7f2e77bc004c851
Emily Knight <emilyknight819@gmail.com>
2024-07-06 14:46:45 -0500

    making it my own app

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 var myapikeys = {
-	google: "AIzaSyDAKGh9hM6lkhtz5MNmuUehgwnvtLVjYr8",
+	google: "AIzaSyDQ_MbsAMQObQjKLGo1hKa2hp9sUGpdu5A",
 	yelp: "E_PPY2I4Kj_RTIlu5iBCzVSdbNhgM8698ypfBPe7-KM3JkJfHtxBjWd5x6KOuvG_2OORfPPl-wb5vpDrVgbttjhU2F7aC2OJATUzhX6CDa0_i5KL2zneMFcyuLUIZXYx",
-	openweather: "2baf085ed1bbea0d1b7d521e3687a9b9",
+	openweather: "0d9571e1b0c1a878cfe2800320169226",
 	unsplash: "hToCtEKI4Nj4xaC0jwg5Tva4DupP-vhbXhJ-mCiry5Y"
 }
 
@@ -12,3 +12,5 @@ var config = {
     "mutChance": 0.2,
     "returnToOrigin": false,
 };
+
+// "AIzaSyDAKGh9hM6lkhtz5MNmuUehgwnvtLVjYr8"
